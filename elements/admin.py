3cfa3db0f204b39fa25b792bcb3b410d8ceccf21
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2023-02-02 14:11:05 +0300

    register Elements, Characteristics, Configuration, Period

--- elements/admin.py
+++ elements/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Elements, Configuration, Characteristics, Period
 
-# Register your models here.
+
+admin.site.register(Elements)
+admin.site.register(Configuration)
+admin.site.register(Characteristics)
+admin.site.register(Period)
