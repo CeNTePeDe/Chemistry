d0b676c8a4d32ba2ea4a1140f8416d7b52298e8a
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2023-02-03 16:00:12 +0300

    add path ElementsAPIView

--- Chemistry/urls.py
+++ Chemistry/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from elements.views import ElementsAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/elements_list/', ElementsAPIView.as_view()),   #(api/версия/название_list)
+    path('api/v1/elements_list/<int:pk>/', ElementsAPIView.as_view()),
+    path('', include('elements.urls')),
 ]
