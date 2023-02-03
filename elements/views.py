fe939286d0b5838e96c9d0a453004b8bd5b0dcf0
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2023-02-03 12:03:36 +0300

    created ElementsAPIView (DRF)

--- elements/views.py
+++ elements/views.py
@@ -1,3 +1,17 @@
+from rest_framework import generics
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from elements.models import Elements
+from elements.serializers import ElementsSerializer
+
+
+class ElementsAPIView(generics.ListAPIView):  # данное представление необходимо связать с определенным маршрутом
+    # определяем отрибут, кторый из таблицы Elements возьмет все записи
+    queryset = Elements.objects.all()
+    # указываем класс сериализатора, его необходимо определить в спец файле
+    serializer_class = ElementsSerializer
+
+
+def index(request):
+    return HttpResponse('Привет')
