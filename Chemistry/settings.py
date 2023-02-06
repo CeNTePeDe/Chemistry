b24433478efeda60cb3cdadb7fccbbd354803eb9
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2023-02-06 11:19:11 +0300

    add REST_FRAMEWORK

--- Chemistry/settings.py
+++ Chemistry/settings.py
@@ -125,3 +125,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDER_CLASSES':[
+        'rest_framework.renders.JSONRenderer',
+        'rest_framework.renders.BrowsableAPIRenderer',
+    ]
+}
