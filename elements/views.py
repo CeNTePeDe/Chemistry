13567d3c4ffa8d57c7a76ca76e1e2593b91d897b
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2023-02-07 11:44:09 +0300

    add methods :get_queryset, element for ElementsViewSet

--- elements/views.py
+++ elements/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.decorators import action
 
 from elements.models import Elements
 from elements.serializers import ElementsSerializer
@@ -12,6 +13,19 @@ from elements.serializers import ElementsSerializer
 class ElementsViewSet(viewsets.ModelViewSet):
     queryset = Elements.objects.all()
     serializer_class = ElementsSerializer
+
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
+        if not 'pk':
+            return Elements.objects.all()[:3]
+        return Elements.objects.filter(pk=pk)
+
+
+    @action(methods=['get'], detail=False)
+    def element(self, request, pk=None):
+        el = Elements.objects.get(pk=pk)
+        return Response({'el':el.name})
+
 
 def index(request):
     return HttpResponse('Привет')
