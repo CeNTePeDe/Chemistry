6935969e201786f5e91eb8d976a2304a8a7c7a76
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2023-02-07 12:09:58 +0300

    add permissions

--- elements/views.py
+++ elements/views.py
@@ -2,6 +2,7 @@ from django.forms import model_to_dict
 from rest_framework import generics, viewsets
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import action
@@ -9,23 +10,18 @@ from rest_framework.decorators import action
 from elements.models import Elements
 from elements.serializers import ElementsSerializer
 
+class ElementsAPIList(generics.ListCreateAPIView):
+    queryset = Elements.objects.all()
+    serializer_class = ElementsSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
-class ElementsViewSet(viewsets.ModelViewSet):
+class ElementsAPIUpdate(generics.RetrieveUpdateAPIView):
     queryset = Elements.objects.all()
     serializer_class = ElementsSerializer
 
-    def get_queryset(self):
-        pk = self.kwargs.get('pk')
-        if not 'pk':
-            return Elements.objects.all()[:3]
-        return Elements.objects.filter(pk=pk)
-
-
-    @action(methods=['get'], detail=False)
-    def element(self, request, pk=None):
-        el = Elements.objects.get(pk=pk)
-        return Response({'el':el.name})
-
-
+class ElementsAPIDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Elements.objects.all()
+    serializer_class = ElementsSerializer
+    permission_classes = (IsAdminUser, )
 def index(request):
     return HttpResponse('Привет')
