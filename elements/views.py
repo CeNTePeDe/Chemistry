652bc6ab7f1eb210b781e4eb2d5eb23210f3b3c9
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2023-02-08 16:24:54 +0300

    add class WomenAPIListPagination

--- elements/views.py
+++ elements/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics, viewsets
 from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -12,10 +13,16 @@ from elements.models import Elements
 from elements.permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from elements.serializers import ElementsSerializer
 
+class WomenAPIListPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
+
 class ElementsAPIList(generics.ListCreateAPIView):
     queryset = Elements.objects.all()
     serializer_class = ElementsSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
+    pagination_class = WomenAPIListPagination
 
 class ElementsAPIUpdate(generics.RetrieveUpdateAPIView):
     queryset = Elements.objects.all()
