4557ea5310fa9ee97f3e1e7108fe68af61d06257
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2023-02-10 15:45:07 +0300

    creat file form, add class ContactForm for contact

---
+++ elements/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class ContactForm(forms.Form):
+    name = forms.CharField(label='Имя', max_length=255)
+    email = forms.EmailField(label='Email')
+    content = forms.CharField(label='Контент', widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}))
+
