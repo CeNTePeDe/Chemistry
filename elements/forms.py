d372f7b83a5e8ed1bf6c2143846d227e91b60be1
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2023-02-14 15:30:53 +0300

    add captcha

--- elements/forms.py
+++ elements/forms.py
@@ -1,7 +1,8 @@
+from captcha.fields import CaptchaField
 from django import forms
 
 class ContactForm(forms.Form):
     name = forms.CharField(label='Имя', max_length=255)
     email = forms.EmailField(label='Email')
     content = forms.CharField(label='Контент', widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}))
-
+    captcha = CaptchaField()
