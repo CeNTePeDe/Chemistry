3aa68c625f1de8c18acac86a3bf94013e5d67ea0
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2023-02-21 15:58:43 +0300

    create class ElementsAdmin in admin.py for element

--- elements/admin.py
+++ elements/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 from .models import Elements, Configuration, Characteristics, Period
 
 
-admin.site.register(Elements)
+
+@admin.register(Elements)
+class ElementsAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'name', 'simbol', 'user')
+    list_filter = ('name',)
+
+
 admin.site.register(Configuration)
 admin.site.register(Characteristics)
 admin.site.register(Period)
