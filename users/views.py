2bb00934dc6f48705e36849f2f40b96f464e8f5a
Olga <olya.labkovich@mail.ru>
2023-03-06 20:14:04 +0300

    add login, registration, profile,logoutto views.py for user app

--- users/views.py
+++ users/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth, messages
+from django.urls import reverse
+
+from users.forms import UserLoggingForm, UserRegistrationForm, UserProfileForm
+
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoggingForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect('/')
+
+
+def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Поздравляем! Регистрация прошла успешно!!!')
+            return HttpResponseRedirect(reverse('user:login'))
+    else:
+        form = UserRegistrationForm()
+    context = {'form': form}
+    return render(request, 'users/registration.html', context)
+
+
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(data=request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect(reverse('users:profile'))
+        else:
+            print(form.errors)
+    else:
+        form = UserProfileForm()
+
+    context = {'form': form}
+    return render(request, 'users/profil.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
