2da56139524048693785dbefe2cad6ebc3be0b09
Olga <olya.labkovich@mail.ru>
2023-03-07 17:34:19 +0300

    urls pattern for create, update, delete

--- elements/urls.py
+++ elements/urls.py
@@ -1,8 +1,17 @@
+from django.conf.urls import url
 from django.urls import path
 
-from elements.views import index,  ElementsDetailView, ElementsListView
+from elements.views import index,  ElementsDetailView, ElementsListView, ElementCreate, ElementsDelete, ElementsUpdate, Search
 
 urlpatterns = [
     path('', ElementsListView.as_view(), name='element_page'),
+    path('search/', Search.as_view(), name='search'),
     path('element/<int:elements_pk>/', ElementsDetailView.as_view(), name='elements-detail'),
+   # path('element/add',  , name='element_add')
+]
+
+urlpatterns += [
+    url(r'element/create/$', ElementCreate.as_view(), name='element_create'),
+    url(r'element/update/(?P<pk>\d+)$', ElementsUpdate.as_view(), name='element_update'),
+    url(r'element/delete/(?P<pk>\d+)$', ElementsDelete.as_view(), name='element_delete'),
 ]
