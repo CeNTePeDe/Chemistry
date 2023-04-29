4737a330e436ae7d3686ea6d5c6a47c4aa492895
Olga Orlova <108580276+CeNTePeDe@users.noreply.github.com>
2023-04-29 11:22:33 +0300

    fix form ContactForm: - added full_name to form

--- elements/forms.py
+++ elements/forms.py
@@ -6,6 +6,7 @@ from elements.models import Elements, Characteristics, Period, Configuration
 
 class ContactForm(forms.Form):
     name = forms.CharField(label='Имя', max_length=255)
+    full_name = forms.CharField(lebel = "Полное имя", max_length=255)
     email = forms.EmailField(label='Email')
     content = forms.CharField(label='Контент', widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}))
     captcha = CaptchaField()
